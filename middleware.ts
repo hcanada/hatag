6a71273802488fdff537478fa663b8b0ff9d6ed9
xerea <harlybusinessmind@gmail.com>
2025-12-23 10:16:07 +0800

    feat: implement Supabase client for browser and server with middleware for authentication

---
+++ middleware.ts
@@ -0,0 +1,50 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll: () => request.cookies.getAll(),
+        setAll: (cookies) =>
+          cookies.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options)
+          ),
+      },
+    }
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // 🚫 If logged in, block /login and /register
+  if (
+    user &&
+    (request.nextUrl.pathname === "/login" ||
+      request.nextUrl.pathname === "/register")
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // If not logged in and trying to access upload
+  if (!user && request.nextUrl.pathname === "/upload") {
+    const loginUrl = new URL("/login", request.url);
+
+    // 👇 remember where they came from
+    loginUrl.searchParams.set("redirect", request.nextUrl.pathname);
+    console.log(loginUrl);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: ["/login", "/register", "/upload"],
+};
